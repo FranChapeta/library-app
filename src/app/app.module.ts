7b34427ae7c73d1e28481acc36d3065c0a62c250
FranChapeta <franchapeta@gmail.com>
2020-11-06 15:41:31 -0300

    Creacion de Componentes y Servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksViewComponent } from './books-view/books-view.component';
+import { BookDetailsComponent } from './books-view/book-details/book-details.component';
+import { BookEditComponent } from './books-view/book-edit/book-edit.component';
+import { AuthorsViewComponent } from './authors-view/authors-view.component';
+import { AuthorEditComponent } from './authors-view/author-edit/author-edit.component';
+import { AuthorDetailsComponent } from './authors-view/author-details/author-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksViewComponent,
+    BookDetailsComponent,
+    BookEditComponent,
+    AuthorsViewComponent,
+    AuthorEditComponent,
+    AuthorDetailsComponent
   ],
   imports: [
     BrowserModule,
